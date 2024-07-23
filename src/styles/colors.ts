83292ba949117beb0366491d2aa56439831a4353
Akuji <akujil26@gmail.com>
2024-07-23 21:39:49 +0200

    dobavil mnogo chego

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,10 +1,14 @@
 interface Colors {
-    primary: string;
-    secondaryBlue: string;
-  }
+  primary: string;
+  secondaryBlue: string;
+  transparentPrimary?: string;
+  transparentSecondaryBlue?: string;
+}
   
-  export const colors: Colors = {
-    primary: "rgb(26, 35, 53)",
-    secondaryBlue: "#1f27f5",
-  };
+export const colors: Colors = {
+  primary: "rgb(26, 35, 53)",
+  secondaryBlue: "#1f27f5",
+  transparentPrimary: "rgba(26, 35, 53, 1)",
+  transparentSecondaryBlue: "rgba(31, 39, 245, 0.5)" 
+};
   
